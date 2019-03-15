f87dd19adde7ea792fd4b130b8c5ce1ed42eee3e
liurongjie <liurongjie@iplas.com.cn>
2019-03-15 19:38:43 +0800

    docker化

--- isuwang-security-app/src/main/java/com/isuwang/security/app/SecurityAppApplication.java
+++ isuwang-security-app/src/main/java/com/isuwang/security/app/SecurityAppApplication.java
@@ -2,12 +2,10 @@ package com.isuwang.security.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@RestController
-public class SecurityAppApplication {
+public class SecurityAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(SecurityAppApplication.class, args);
