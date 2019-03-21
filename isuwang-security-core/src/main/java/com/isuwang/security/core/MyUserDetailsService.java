4227597d01452a2421ff909bb3d14d57063d336c
liurongjie <liurongjie@iplas.com.cn>
2019-03-21 17:09:24 +0800

    添加dapeng 引用

--- isuwang-security-core/src/main/java/com/isuwang/security/core/MyUserDetailsService.java
+++ isuwang-security-core/src/main/java/com/isuwang/security/core/MyUserDetailsService.java
@@ -1,6 +1,9 @@
 package com.isuwang.security.core;
 
+import com.github.dapeng.core.SoaException;
 import com.isuwang.security.core.service.UserService;
+import com.isuwang.soa.crmdb.customer.CustomerServiceClient;
+import com.isuwang.soa.crmdb.customer.domain.TCustomer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +33,13 @@ public class MyUserDetailsService implements UserDetailsService , SocialUserDeta
         //根据用户名查找用户信息
         logger.info("form user login name:"+username);
 
-        return buildUser(username);
+        UserDetails userDetails = null;
+        try {
+            userDetails =  buildUser(username);
+        } catch (SoaException e) {
+            logger.error(e.getMsg(),e);
+        }
+        return userDetails;
     }
 
     @Override
@@ -38,18 +47,27 @@ public class MyUserDetailsService implements UserDetailsService , SocialUserDeta
         //根据用户名查找用户信息
         logger.info("socile user login userId:"+userId);
 
-        return buildUser(userId);
+        SocialUserDetails userDetails = null;
+        try {
+            userDetails =  buildUser(userId);
+        } catch (SoaException e) {
+            logger.error(e.getMsg(),e);
+        }
+
+        return userDetails;
     }
 
-    private SocialUserDetails buildUser(String username) {
+    private SocialUserDetails buildUser(String username) throws SoaException {
 //        // TODO 模拟数据库登录方式
 //        logger.info("登录用户名：" + userId);
 //        String password = passwordEncoder.encode("123456");
 ////        String password = "e10adc3949ba59abbe56e057f20f883e";
+
+        TCustomer customer =new CustomerServiceClient().findCustomerByLoginName(username);
         User user = userService.getUserByUserName(username);
         logger.info("数据库密码是：" + user.getPassword());
-        return new SocialUser(username, user.getPassword(),
-                true, true, true, true,
+        return new SocialUser(username, customer.password,
+                customer.disable == 0 ? true : false, true, true, true,
                 AuthorityUtils.commaSeparatedStringToAuthorityList("admin,ROLE_USER"));
     }
 }
