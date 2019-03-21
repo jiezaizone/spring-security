5984bddcc642a6987951e9a030882b17f7f17318
liurongjie <liurongjie@iplas.com.cn>
2019-03-21 17:49:32 +0800

    添加短信登录校验实现，调用dapeng

--- isuwang-security-core/src/main/java/com/isuwang/security/core/vaildate/code/sms/SmsValidateCodeProcessor.java
+++ isuwang-security-core/src/main/java/com/isuwang/security/core/vaildate/code/sms/SmsValidateCodeProcessor.java
@@ -1,9 +1,18 @@
 package com.isuwang.security.core.vaildate.code.sms;
 
+import com.github.dapeng.core.SoaException;
+import com.isuwang.security.core.properties.SecurityConstants;
 import com.isuwang.security.core.vaildate.code.ValidateCode;
 import com.isuwang.security.core.vaildate.code.AbstractValidateCodeProcessor;
+import com.isuwang.security.core.vaildate.code.ValidateCodeException;
+import com.isuwang.soa.crmdb.company.domain.wechatcustomer.LoginResponse;
+import com.isuwang.soa.crmdb.company.domain.wechatcustomer.WechatLoginByVerifyCodeRequest;
+import com.isuwang.soa.crmdb.wechatcustomer.WechatCustomerBizServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
 
@@ -13,6 +22,9 @@ import org.springframework.web.context.request.ServletWebRequest;
  */
 @Component("smsValidateCodeProcessor")
 public class SmsValidateCodeProcessor extends AbstractValidateCodeProcessor<ValidateCode> {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private SmsCodeSender smsCodeSender;
 
@@ -26,7 +38,35 @@ public class SmsValidateCodeProcessor extends AbstractValidateCodeProcessor<Vali
     // 重写短信验证码校验方法 （改用dapeng接口调用）
     @Override
     public void validate(ServletWebRequest request) {
-//        super.validate(request);
+        String mobile = "";
+        String smsCode = "";
+        String openId = "";
+        try {
+            mobile = ServletRequestUtils.getStringParameter(request.getRequest(), SecurityConstants.DEFAULT_PARAMETER_NAME_MOBILE);
+        } catch (ServletRequestBindingException e) {
+            logger.error(e.getMessage(),e);
+            throw new ValidateCodeException("获取手机号码的值失败");
+        }
+        try {
+            smsCode = ServletRequestUtils.getStringParameter(request.getRequest(), SecurityConstants.DEFAULT_PARAMETER_NAME_CODE_SMS);
+        } catch (ServletRequestBindingException e) {
+            logger.error(e.getMessage(),e);
+            throw new ValidateCodeException("获取验证码的值失败");
+        }
+        try {
+            openId = ServletRequestUtils.getStringParameter(request.getRequest(), SecurityConstants.DEFAULT_PARAMETER_NAME_OPENID);
+        } catch (ServletRequestBindingException e) {
+            logger.error(e.getMessage(),e);
+        }
+
+        try {
+            WechatLoginByVerifyCodeRequest verifyCodeRequest = new WechatLoginByVerifyCodeRequest();
+            verifyCodeRequest.phone(mobile).verifyCode(smsCode).wechatOpenid(openId);
+            LoginResponse loginResponse = new WechatCustomerBizServiceClient().verifyCodeLogin(verifyCodeRequest);
+        } catch (SoaException e) {
+            logger.error(e.getMessage(),e);
+            throw new ValidateCodeException("短信登录校验失败");
+        }
         // 短信验证码不校验，默认通过
     }
 }
